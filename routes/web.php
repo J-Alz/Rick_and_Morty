43d62915dc8756a9f8401525a4024e1e2bdfbbee
John Hilasaca <89051327+Jaalya@users.noreply.github.com>
2023-08-02 21:29:46 -0500

    change README.md

--- routes/web.php
+++ routes/web.php
@@ -2,21 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
 Route::get('/', function () {
     return view('welcome');
 });
-//Route::get('/', function(){return view('components.board');});
+
 /* SHOWCONTROLLER */
 route::get('/',[App\Http\Controllers\showController::class, 'showDataInView']);
 route::get('/human',[App\Http\Controllers\showController::class, 'showSpecieHuman']);
@@ -30,6 +19,6 @@ route::get('/cronenberg',[App\Http\Controllers\showController::class, 'showSpeci
 route::get('/search',[App\Http\Controllers\showController::class, 'showSearch'])->name('showSearch');
 
 route::get('/character/{id}',[App\Http\Controllers\showController::class,'showCharacterDetail'])->name('characterDetail');
-//Route::get('/all', [App\Http\Controllers\ApiController::class, 'index']);
+
 /* APICONTROLLER */
 Route::get('/consume-api', [App\Http\Controllers\ApiController::class, 'consumeAPI_saveAPI']);
